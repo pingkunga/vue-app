b81d56ce485709daa01f9a83382f4931113e11f6
Chatri Ngambenchawong <pingkunga@gmail.com>
2023-09-24 17:35:10 +0700

    add support automate deploy with docker server

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-app/',
+  //For GitHub Pages
+  //base: '/vue-app/',
   plugins: [
     vue(),
   ],
